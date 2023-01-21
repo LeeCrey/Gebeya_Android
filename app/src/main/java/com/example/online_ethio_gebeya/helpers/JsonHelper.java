10a10af8495835df10f95bd6da484ab03152f547
LeeCrey <SolomonBoloshe11x@gmail.com>
2023-01-21 19:48:46 +0300

    removed sign up deserializer

--- app/src/main/java/com/example/online_ethio_gebeya/helpers/JsonHelper.java
+++ app/src/main/java/com/example/online_ethio_gebeya/helpers/JsonHelper.java
@@ -1,15 +1,11 @@
 package com.example.online_ethio_gebeya.helpers;
 
 import com.example.online_ethio_gebeya.models.Customer;
-import com.example.online_ethio_gebeya.models.responses.RegistrationResponse;
 import com.example.online_ethio_gebeya.models.responses.SessionResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonHelper {
-    public static RegistrationResponse parseAccountError(String message) throws JsonProcessingException {
-        return new ObjectMapper().readerFor(RegistrationResponse.class).readValue(message);
-    }
 
     public static SessionResponse parseLoginError(String message) throws JsonProcessingException {
         return new ObjectMapper().readerFor(SessionResponse.class).readValue(message);
@@ -17,9 +13,5 @@ public class JsonHelper {
 
     public static Customer parseCustomer(String message) throws JsonProcessingException {
         return new ObjectMapper().readerFor(Customer.class).readValue(message);
-    }
-
-    public static RegistrationResponse parseSignUpError(String error) throws JsonProcessingException {
-        return new ObjectMapper().readerFor(RegistrationResponse.class).readValue(error);
     }
 }
