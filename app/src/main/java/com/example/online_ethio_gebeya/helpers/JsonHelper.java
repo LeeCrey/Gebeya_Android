308d09d149e9ce44f03d44370a8cfa61531ca2e0
LeeCrey <SolomonBoloshe11x@gmail.com>
2023-01-22 13:31:32 +0300

    parse operation error method

--- app/src/main/java/com/example/online_ethio_gebeya/helpers/JsonHelper.java
+++ app/src/main/java/com/example/online_ethio_gebeya/helpers/JsonHelper.java
@@ -1,6 +1,7 @@
 package com.example.online_ethio_gebeya.helpers;
 
 import com.example.online_ethio_gebeya.models.Customer;
+import com.example.online_ethio_gebeya.models.responses.InstructionsResponse;
 import com.example.online_ethio_gebeya.models.responses.SessionResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,5 +14,9 @@ public class JsonHelper {
 
     public static Customer parseCustomer(String message) throws JsonProcessingException {
         return new ObjectMapper().readerFor(Customer.class).readValue(message);
+    }
+
+    public static InstructionsResponse parseOperationError(String error) throws JsonProcessingException {
+        return new ObjectMapper().readerFor(InstructionsResponse.class).readValue(error);
     }
 }
