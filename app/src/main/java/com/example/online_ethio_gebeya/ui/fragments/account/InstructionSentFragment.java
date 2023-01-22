7fea3d2d2940da1330a35965d48765237d22e36e
LeeCrey <SolomonBoloshe11x@gmail.com>
2023-01-22 09:28:14 +0300

    modified

--- app/src/main/java/com/example/online_ethio_gebeya/ui/fragments/account/InstructionSentFragment.java
+++ app/src/main/java/com/example/online_ethio_gebeya/ui/fragments/account/InstructionSentFragment.java
@@ -5,16 +5,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
-import com.example.online_ethio_gebeya.R;
+import com.example.online_ethio_gebeya.callbacks.MainActivityCallBackInterface;
+import com.example.online_ethio_gebeya.databinding.FragmentInstructionSentBinding;
 
 public class InstructionSentFragment extends Fragment {
+    private FragmentInstructionSentBinding binding;
+    private NavController navController;
+    public static final String MESSAGE = "message";
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instruction_sent, container, false);
+        binding = FragmentInstructionSentBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        navController = Navigation.findNavController(view);
+
+        String msg = InstructionSentFragmentArgs.fromBundle(getArguments()).getMessage();
+        binding.message.setText(msg);
+
+        // event
+        binding.openEmail.setOnClickListener(v -> ((MainActivityCallBackInterface) requireActivity()).openEmailApp());
+        binding.skipIWillConfirm.setOnClickListener(v -> navController.navigateUp());
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        binding = null;
+        navController = null;
     }
 }
