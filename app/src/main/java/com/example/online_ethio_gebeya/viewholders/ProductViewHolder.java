4aed4287f2d4b6d6a053c7c48d5a0b100a0ce410
lee_crey_116 <solomonboloshe835@gmail.com>
2023-02-22 13:42:05 +0300

    glide

--- app/src/main/java/com/example/online_ethio_gebeya/viewholders/ProductViewHolder.java
+++ app/src/main/java/com/example/online_ethio_gebeya/viewholders/ProductViewHolder.java
@@ -1,19 +1,14 @@
 package com.example.online_ethio_gebeya.viewholders;
 
 import android.content.Context;
-import android.graphics.Paint;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.databinding.BindingAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.RequestManager;
 import com.example.online_ethio_gebeya.R;
 import com.example.online_ethio_gebeya.databinding.LayoutProductBinding;
 import com.example.online_ethio_gebeya.models.Product;
-import com.squareup.picasso.Picasso;
 
 public class ProductViewHolder extends RecyclerView.ViewHolder {
     private final LayoutProductBinding productBinding;
@@ -25,12 +20,15 @@ public class ProductViewHolder extends RecyclerView.ViewHolder {
     }
 
     // if it is search layout attach only image.
-    public void bindView(final Context context, final Product product) {
+    public void bindView(final @NonNull Context context, final Product product, @NonNull RequestManager glide) {
         productBinding.setProduct(product);
         Double currentPrice = product.getPrice();
         if (product.getDiscount() != null) {
             currentPrice = product.getPrice() - product.getDiscount();
         }
+
         productBinding.setPrice(context.getString(R.string.price_in_ethio, currentPrice));
+
+        glide.load(product.getThumbnail()).centerCrop().placeholder(R.drawable.load_error).into(productBinding.productImage);
     }
 }
