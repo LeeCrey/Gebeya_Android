24fc3edafa63d3b9a2916311052428e1fcaca2f7
lee_crey_116 <solomonboloshe835@gmail.com>
2023-02-22 19:59:03 +0300

    removed tag

--- app/src/main/java/com/example/online_ethio_gebeya/viewholders/CheckoutItemViewHolder.java
+++ app/src/main/java/com/example/online_ethio_gebeya/viewholders/CheckoutItemViewHolder.java
@@ -1,7 +1,5 @@
 package com.example.online_ethio_gebeya.viewholders;
 
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,7 +10,6 @@ import com.example.online_ethio_gebeya.models.CartItem;
 
 public class CheckoutItemViewHolder extends RecyclerView.ViewHolder {
     private final LayoutModalItemBinding binding;
-    private static final String TAG = "CheckoutItemViewHolder";
 
     public CheckoutItemViewHolder(@NonNull LayoutModalItemBinding _binding) {
         super(_binding.getRoot());
@@ -23,7 +20,6 @@ public class CheckoutItemViewHolder extends RecyclerView.ViewHolder {
     public void bindView(@NonNull CartItem item, @NonNull RequestManager glide) {
         binding.setCartItem(item);
 
-        Log.d(TAG, "bindView: " + item.getItemImage());
         glide.load(item.getItemImage()).centerCrop().placeholder(R.drawable.load_error).into(binding.productImage);
     }
 }
