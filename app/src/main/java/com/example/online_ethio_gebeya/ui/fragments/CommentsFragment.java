7141875ad40c91467b78e18ac6a3bf255817d861
lee_crey_116 <solomonboloshe835@gmail.com>
2023-02-23 11:37:18 +0300

    created

---
+++ app/src/main/java/com/example/online_ethio_gebeya/ui/fragments/CommentsFragment.java
@@ -0,0 +1,36 @@
+package com.example.online_ethio_gebeya.ui.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.online_ethio_gebeya.databinding.FragmentCommentsBinding;
+
+public class CommentsFragment extends Fragment {
+    private FragmentCommentsBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentCommentsBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // get list of comments
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        binding = null;
+    }
+}
