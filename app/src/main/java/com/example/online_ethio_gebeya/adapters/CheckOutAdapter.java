003da1054678ff69c4b4909fe538e76e43c72e6b
lee_crey_116 <solomonboloshe835@gmail.com>
2023-02-24 21:21:59 +0300

    modified CheckOutAdapter

--- app/src/main/java/com/example/online_ethio_gebeya/adapters/CheckOutAdapter.java
+++ app/src/main/java/com/example/online_ethio_gebeya/adapters/CheckOutAdapter.java
@@ -1,32 +1,31 @@
 package com.example.online_ethio_gebeya.adapters;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.ListAdapter;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
 import com.example.online_ethio_gebeya.R;
 import com.example.online_ethio_gebeya.databinding.LayoutModalItemBinding;
+import com.example.online_ethio_gebeya.helpers.diff_calc.ItemCallBack;
 import com.example.online_ethio_gebeya.models.Item;
 import com.example.online_ethio_gebeya.viewholders.CheckoutItemViewHolder;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class CheckOutAdapter extends RecyclerView.Adapter<CheckoutItemViewHolder> {
-    private List<Item> itemList;
+public class CheckOutAdapter extends ListAdapter<Item, CheckoutItemViewHolder> {
     private final LayoutInflater inflater;
     private final RequestManager glide;
 
-    public CheckOutAdapter(@NonNull Context context, List<Item> itemList) {
+    public CheckOutAdapter(@NonNull Context context) {
+        super(new ItemCallBack());
+
         inflater = LayoutInflater.from(context);
-        this.itemList = itemList;
         glide = Glide.with(context);
     }
 
@@ -40,21 +39,14 @@ public class CheckOutAdapter extends RecyclerView.Adapter<CheckoutItemViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull CheckoutItemViewHolder holder, int position) {
-        holder.bindView(itemList.get(position), glide);
+        holder.bindView(getItem(position), glide);
     }
 
-    @Override
-    public int getItemCount() {
-        return itemList == null ? 0 : itemList.size();
-    }
-
-    @SuppressLint("NotifyDataSetChanged")
     public void setList(List<Item> _itemList) {
         if (_itemList == null) {
             return;
         }
 
-        itemList = new ArrayList<>(_itemList);
-        notifyDataSetChanged();
+        submitList(_itemList);
     }
 }
